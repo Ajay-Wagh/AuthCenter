596885924e4d160e9447f3e00187a67dddd5a669
phoenix <awempires@gmail.com>
2022-08-27 18:52:01 +0530

    Interceptor modified, Logging improved, Integrity check added

--- src/main/java/com/ajaywagh/authcenter/endpoints/AdminEndpoints.java
+++ src/main/java/com/ajaywagh/authcenter/endpoints/AdminEndpoints.java
@@ -1,14 +1,18 @@
 package com.ajaywagh.authcenter.endpoints;
 
 import com.ajaywagh.authcenter.requestmodels.admin.AddAdminRequest;
+import com.ajaywagh.authcenter.requestmodels.admin.RemoveAdminRequest;
 import com.ajaywagh.authcenter.responsemodels.admin.AdminResponse;
 import com.ajaywagh.authcenter.responsemodels.Success;
 import com.ajaywagh.authcenter.services.admin.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 
@@ -18,9 +22,15 @@ public class AdminEndpoints {
     @Autowired
     AdminService adminService;
 
-    @RequestMapping("/admin/add")
+
+    @RequestMapping("/add")
     public AdminResponse addAdmin(@Valid  @RequestBody AddAdminRequest addAdminRequest){
         return adminService.add(addAdminRequest);
+    }
+
+    @RequestMapping("/remove")
+    public AdminResponse removeAdmin(RemoveAdminRequest removeAdminRequest){
+        return null;
     }
 
 }
